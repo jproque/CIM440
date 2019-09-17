82f2a5fc0676089381a76d7c63d0d4af62cb267c
jproque <johnpaulroq@gmail.com>
2019-09-17 12:12:57 -0400

    update showhide

--- classexamples/showhide/sketch.js
+++ classexamples/showhide/sketch.js
@@ -2,21 +2,35 @@ var puppies;
 
 var showOrHide = false;
 
+var show;
+var hide;
+
 function preload(){
 
   puppies = loadImage("puppies.jpg");
 
+  show = createButton("Show Puppy");
+  show.mousePressed(function(){
+    showOrHide = true;
+  });
+
+  hide = createButton("Hide Puppy");
+  hide.mousePressed(function(){
+    showOrHide = false;
+  });
+
 }
-
-
-
 
 function setup() {
   createCanvas(400,400);
 }
 
 function draw() {
+  background(255);
 
-  image(puppies, 100,100, puppies.width/4, puppies.height/4);
+  if(showOrHide == true){
+    image(puppies, 100,100, puppies.width/4, puppies.height/4);
+  }
+
 
 }
