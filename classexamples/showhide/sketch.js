745cef693555940cb9f1db3652e22a4708975bba
jproque <johnpaulroq@gmail.com>
2019-09-17 12:05:24 -0400

    create show hide

--- classexamples/showhide/sketch.js
+++ classexamples/showhide/sketch.js
@@ -1,5 +1,7 @@
 var puppies;
 
+var showOrHide = false;
+
 function preload(){
 
   puppies = loadImage("puppies.jpg");
@@ -10,14 +12,11 @@ function preload(){
 
 
 function setup() {
-  // put setup code here
   createCanvas(400,400);
 }
 
 function draw() {
-  // put drawing code here
-  image(puppies,0,0);
+
   image(puppies, 100,100, puppies.width/4, puppies.height/4);
-  //set exact dimensions
-  image(puppies,200,200,125,200);
+
 }
