0f79b9923f1a25814ae65c0d0fc901b10c99183b
jproque <johnpaulroq@gmail.com>
2019-10-16 17:03:27 -0400

    midterm and classwork

--- classexamples/seasons/sketch.js
+++ classexamples/seasons/sketch.js
@@ -1,6 +1,24 @@
 var seasonSelect;
 var seasonType = "";
 
+var sunX = 0;
+var sunY = 0;
+
+var snowX = [];
+var snowY = [];
+
+var snowAmount = 100;
+
+var leafX = [];
+var leafY = [];
+var leafAmount = 100;
+var leafImage;
+
+function preload(){
+  leafImage = loadImage("fall-leaf.png");
+
+}; // end of preLoad
+
 function setup() {
   createCanvas(400,400);
 
@@ -19,6 +37,29 @@ function setup() {
   sunX = width/2;
   sunY = height * 2;
 
+  //var i = 0 is our start point
+  // i < 100 is our conditional - how many times do we want the for loop to background
+  //i++ is the same as saying i=i+1 - also called the iterator
+  for(var i = 0; i < snowAmount; i ++){
+    snowX[i] = random(0,width); //random number is from 0 to width - 1
+    snowY[i] = random(0,-500); //random number is from 0 to -500 (above the canvas)
+
+  };
+
+  // var i is destryyed after for loop is done
+
+  console.log("snowX " + snowX);
+  console.log("snowY " + snowY);
+
+  for(var i = 0; i < leafAmount; i++){
+    leafX[i] = random(0,width);
+    leafY[i] = random(0, -500);
+
+  };
+
+  console.log("leafX " + leafX);
+  console.log("leafY " + leafY);
+
 } // end of setup
 
 function draw() {
@@ -26,15 +67,39 @@ function draw() {
 
   if(seasonType == "Fall"){
     console.log("fall");
+
+    for(var f = 0; f < snowAmount; f++){
+      if(leafY[f] < height - 20){
+        leafY[f]++;
+        leafX[f] = leafX[f] + sin(radians(frameCount));
+      };
+
+      image(leafImage, leafX[f], leafY[f], 20, 20);
+    };
+
   }else if (seasonType == "Winter") {
     console.log("Winter");
+
+    for(var y = 0; y < snowAmount; y++){
+      snowY[y]++; //same as snowY[y] = snowY[y] + 1
+      //snowY[y] = snowY[y] + 5 //increases the speed at which the snow falls
+      fill("white");
+      stroke("black");
+      ellipse(snowX[y], snowY[y], 10,10);
+
+      if(snowY[y] > height){
+        snowY[y] = random(0,-500);
+        snowX[y] = random(0,width);
+      };
+    };
+
   }else if (seasonType == "Spring") {
     console.log("Spring");
   }else if (seasonType == "Summer") {
     console.log("Summer");
     fill("orange");
     stroke("red");
-    ellipse(sunX,sunY,width,height);y
+    ellipse(sunX,sunY,width,height);
 
     if(sunY > height){
       sunY = sunY - 1;
---
+++ hw/midterm-project/sketch.js
@@ -0,0 +1,158 @@
+var ballX = 0;
+var ballY = 0;
+var ballImage;
+var hoopHeight = 0;
+var hoopImage;
+var playerImage;
+var ballMove = false;
+
+var hitX = 345;
+var hitW = 60;
+var hitH = 120;
+
+var offset = 0;
+
+var ballAim = 0;
+
+var score = 1;
+var pointScored = false;
+
+function preload(){
+  ballImage = loadImage("basketball.png");
+
+  hoopImage = loadImage("basketball-hoop-1.png");
+
+  playerImage = loadImage("basketball-player.png");
+
+}; // end of preLoad
+
+function setup() {
+  createCanvas(900,400);
+
+    ballX = 0;
+    ballY = height/2 - 10;
+
+    ballAim = height/2;
+
+} // end of setup
+
+function draw() {
+  background(255);
+
+  // instructions
+  fill(255);
+  stroke(0);
+  rect(435, 200, 275, 140);
+  textSize(12);
+  stroke(255);
+  let spacebar = 'Press SPACE bar to shoot ball.';
+  let arrows = 'Press UP & DOWN ARROWS to aim the ball.';
+  let reset = 'Press R to reset the ball after the shot.';
+  fill(0);
+  text(arrows, 450, 275);
+  text(spacebar, 450, 225);
+  text(reset, 450, 325);
+  // end of instructions
+
+  // basketball hoop
+  image(hoopImage, 345, hoopHeight, 60, 120);
+
+  // hoop hitzone
+  if(ballX > hitX && ballX < hitX+hitW && ballY > hoopHeight && ballY < hoopHeight+hitH){
+    console.log("In basket");
+    score = score + 1;
+  }
+  // end of hoop hitzone
+
+  //scoreboard
+  fill(0);
+  rect(435, 25, 275, 140);
+  fill(255);
+  textSize(20);
+  stroke(255);
+  let scoreboard = 'YOUR SCORE';
+  text(scoreboard,500,50);
+  textSize(60);
+  // let score = 1;
+  text(score,550,120);
+  // end of scoreboard
+
+  // basketball & aiming line
+  stroke(0);
+  line(30, height/2 , 80, ballAim);
+  image(ballImage, ballX+20, ballY, 20, 20);
+
+  // basketball playerImage
+  image(playerImage, 0, 195, 50, 150);
+
+  // ball movement
+  if(ballMove == true){
+    ballX++;
+    if (offset < 0) {
+      ballY = ballY + offset;
+    }else {
+      ballY = ballY + offset;
+    }
+
+    console.log("offset " + offset);
+    console.log("BallY " + ballY);
+    console.log("BallX " + ballX);
+
+  }
+  // end of ballMove true
+
+  // ball stop moving
+  if(ballX > width-530){
+    ballMove = false;
+  }
+
+  if(ballY > height - 20){
+    ballMove = false;
+  }
+
+  if(ballY < 0){
+    ballMove = false;
+  }
+  // end of ballMove false
+
+} // end of draw
+
+function keyPressed(){
+
+  console.log("key " + key);
+  console.log("keyCode " + keyCode);
+
+  // SPACE BAR
+  if (keyCode == 32) {
+    ballMove = true;
+
+  } // end of space bar
+
+  // 'R' RESET
+  if(key == 'r' && ballMove == false){
+    ballX = 0;
+    ballY = height/2 - 10;
+    ballMove = false;
+    offset = 0;
+    ballAim = height/2;
+    hoopHeight = random(0, 280);
+  } // end of 'r'
+
+  // UP ARROW
+  if(keyCode == 38 && ballMove == false){
+    offset = offset - 0.1;
+    ballAim = ballAim - 5.5;
+    console.log("offset " + offset);
+    console.log("ball aim " + ballAim);
+
+  } // end of up arrow
+
+  // DOWN ARROW
+  if(keyCode == 40 && ballMove == false){
+    offset = offset + 0.1;
+    ballAim = ballAim + 5.5;
+    console.log("offset " + offset);
+    console.log("ball aim " + ballAim);
+  } // end of down arrow
+
+}; // end of keyPressed
