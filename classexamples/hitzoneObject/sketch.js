7ba8882c7706d6c5ce170f22606cf933efab122b
jproque <johnpaulroq@gmail.com>
2019-10-29 12:03:39 -0400

    hitzoneObject example work

--- classexamples/hitzoneObject/sketch.js
+++ classexamples/hitzoneObject/sketch.js
@@ -4,9 +4,9 @@ var button2;
 function setup() {
 createCanvas(400,400);
 
-button1 = new hitzoneObject(100, 200, 50, "red");
+button1 = new hitzoneObject(100, 200, 50, "red", "Stop", loadImage("assets/stop.png"));
 
-button2 = new hitzoneObject(200, 200, 50, "green");
+button2 = new hitzoneObject(200, 200, 50, "green", "Play", loadImage("assets/play.png"));
 
 } // end of setup
 
@@ -19,39 +19,39 @@ function draw() {
   button2.display();
   button2.check();
 
-    break;
-
 } // end of draw
 
 class hitzoneObject{
 
-  constructor(tempX, tempY, tempSize, tempColor){
+  constructor(tempX, tempY, tempSize, tempColor, tempLabel, tempImage){
     this.x = tempX;
     this.y = tempY;
     this.boxSize = tempSize;
     this.boxColor = tempColor;
     this.overlay = false;
+    this.label = tempLabel;
+    this.image = tempImage;
   } // end of constructor
 
   display(){
     fill(this.boxColor);
-    rect(this.x, this.y, this.boxSize, this.boxSize);
-  }
+    text(this.label, this.x, this.y - 20);
+    image(this.image,this.x, this.y, this.boxSize, this.boxSize);
+
+    if(this.overlay == true){
+      fill(127,127);
+      rect(this.x, this.y, this.boxSize, this.boxSize);
+    }
+  } // end of display function
 
   check(){
-    if(mouseX > ths.x && mouseX < (this.x + this.boxSize) && mouseY > this.y && mouseY < (this.y + this.boxSize)){
-      console.log("over button" + this.boxColor);
-
+    if(mouseX > this.x && mouseX < (this.x + this.boxSize) && mouseY > this.y && mouseY < (this.y + this.boxSize)){
+      // console.log("over button" + this.boxColor);
       this.overlay = true;
-
     }else {
-      console.log("off button" + this.boxColor);
-
+      // console.log("off button" + this.boxColor);
       this.overlay = false;
-
-
     }
-
-  }
+  } // end of check function
 
 } // end of hitzoneObject
